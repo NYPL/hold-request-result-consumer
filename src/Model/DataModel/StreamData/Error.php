c5e68181ae8713b8a719fa4bb5ee69d8eac0749e
Ho-Ling Poon <holingpoon@nypl.org>
2017-06-22 11:43:09 -0400

    REQ-56 Adding Error class part of HoldRequestResult.

---
+++ src/Model/DataModel/StreamData/Error.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: holingpoon
+ * Date: 6/21/17
+ * Time: 3:55 PM
+ */
+
+namespace NYPL\HoldRequestResultConsumer\Model\DataModel\StreamData;
+
+use NYPL\HoldRequestResultConsumer\Model\DataModel\StreamData;
+
+/**
+ * Class Error
+ * @package NYPL\HoldRequestResultConsumer\Model\DataModel\StreamData
+ */
+class Error extends StreamData
+{
+    /**
+     * @var string
+     */
+    public $type = '';
+
+    /**
+     * @var string
+     */
+    public $message = '';
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType(string $type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage(string $message)
+    {
+        $this->message = $message;
+    }
+ }
