363755b945a2414f586c3e129f4846a300bf1c27
Ho-Ling Poon <holingpoon@nypl.org>
2017-07-06 15:04:27 -0400

    REQ-55 Adding APIExceptions.

--- src/OAuthClient/HoldRequestClient.php
+++ src/OAuthClient/HoldRequestClient.php
@@ -2,8 +2,10 @@
 namespace NYPL\HoldRequestResultConsumer\OAuthClient;
 
 use NYPL\HoldRequestResultConsumer\Model\DataModel\HoldRequest;
+use NYPL\Starter\APIException;
 use NYPL\Starter\APILogger;
 use NYPL\Starter\Config;
+use NYPL\Starter\Model\Response\ErrorResponse;
 
 class HoldRequestClient extends APIClient
 {
@@ -26,8 +28,48 @@ class HoldRequestClient extends APIClient
         return new HoldRequest($response['data']);
     }
 
-    public static function patchHoldRequestById($holdRequestId = '', bool $processed = false, bool $success = false)
+    /**
+     * @param string $holdRequestId
+     * @param bool $processed
+     * @param bool $success
+     * @return HoldRequest
+     * @throws APIException
+     */
+    public static function patchHoldRequestById($holdRequestId = '', bool $processed, bool $success)
     {
+        if ($holdRequestId === '') {
+            throw new APIException(
+                'No Hold Request Id.',
+                'No Hold Request Id provided.',
+                0,
+                null,
+                500,
+                new ErrorResponse(500, 'no-hold-request-id', 'No Hold Request Id provided.')
+            );
+        }
+
+        if (!isset($processed)) {
+            throw new APIException(
+                'Processed flag not set',
+                'Processed flag is not set.',
+                0,
+                null,
+                500,
+                new ErrorResponse(500, 'processed-flag-not-set', 'Processed flag is not set.')
+            );
+        }
+
+        if (!isset($success)) {
+            throw new APIException(
+                'Success flag not set',
+                'Success flag is not set.',
+                0,
+                null,
+                500,
+                new ErrorResponse(500, 'success-flag-not-set', 'Success flag is not set.')
+            );
+        }
+
         $url = Config::get('API_HOLD_REQUEST_URL') . '/' . $holdRequestId;
 
         APILogger::addInfo('Patching hold request by id', $url);
