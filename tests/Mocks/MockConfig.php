cb7864f1769c17b4df02041025645058cb17e547
Ho-Ling Poon <holingpoon@nypl.org>
2017-08-11 15:11:07 -0400

    Change to available env.sample file.

--- tests/Mocks/MockConfig.php
+++ tests/Mocks/MockConfig.php
@@ -8,7 +8,7 @@ use NYPL\Starter\Config;
 
 class MockConfig extends Config
 {
-    const LOCAL_ENVIRONMENT_FILE = '.env';
+    const LOCAL_ENVIRONMENT_FILE = '.env.sample';
     const GLOBAL_ENVIRONMENT_FILE = 'var_app';
     const DEFAULT_TIME_ZONE = 'America/New_York';
 
