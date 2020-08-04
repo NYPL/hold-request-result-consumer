c5c383aa603cbded01c9af539669b58acc150de8
Paul Beaudoin <contact@paulbeaudoin.com>
2020-08-03 21:35:28 -0400

    Fix travis deploy (cleanup)

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -22,9 +22,6 @@ DEPLOY_CMD=$(cat package.json | jq ".scripts | .\"deploy-$ENVIRONMENT_NAME\"" --
 # known to be set in travis:
 DEPLOY_CMD=$(echo "$DEPLOY_CMD" | sed "s/ --profile [a-z-]*/ -a \$AWS_ACCESS_KEY_ID_$ENVIRONMENT_CAPS -s \$AWS_SECRET_ACCESS_KEY_$ENVIRONMENT_CAPS/g")
 
-echo "node version:"
-echo $(node --version)
-
 echo Running deploy command for "$ENVIRONMENT_NAME" environment:
 echo "  $DEPLOY_CMD"
 
