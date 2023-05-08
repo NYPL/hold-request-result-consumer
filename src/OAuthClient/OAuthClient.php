6263ed814d3703863dbe8c5949600e054857f583
Paul Beaudoin <contact@paulbeaudoin.com>
2023-05-08 13:37:21 -0400

    Remove multi-base-url support in OAuthClient

--- src/OAuthClient/OAuthClient.php
+++ src/OAuthClient/OAuthClient.php
@@ -4,54 +4,48 @@ namespace NYPL\HoldRequestResultConsumer\OAuthClient;
 use League\OAuth2\Client\Provider\GenericProvider;
 use League\OAuth2\Client\Token\AccessToken;
 use NYPL\Starter\Config;
-use NYPL\Starter\APILogger;
 
 class OAuthClient
 {
     /**
      * @var string
      */
-    public static $accessToken = array(); // '';
+    public static $accessToken = '';
 
     /**
      * @return string
      */
-    public static function getAccessToken($configPrefix = '')
+    public static function getAccessToken()
     {
-        // APILogger::addInfo("OauthClient::getAccessToken('$configPrefix')");
-        if (!array_key_exists($configPrefix, self::$accessToken)) {
-            // APILogger::addInfo("OauthClient::getAccessToken('$configPrefix') not yet built");
-            $accessToken = self::initializeAccessToken($configPrefix);
-            // APILogger::addInfo("Got access token: '{$accessToken->getToken()}'");
+        if (!self::$accessToken) {
+            $accessToken = self::initializeAccessToken();
 
-            self::setAccessToken($accessToken->getToken(), $configPrefix);
+            self::setAccessToken($accessToken->getToken());
         }
 
-        return self::$accessToken[$configPrefix];
+        return self::$accessToken;
     }
 
     /**
      * @param string $accessToken
      */
-    public static function setAccessToken($accessToken, $configPrefix)
+    public static function setAccessToken($accessToken)
     {
-        self::$accessToken[$configPrefix] = $accessToken;
+        self::$accessToken = $accessToken;
     }
 
-    protected static function initializeAccessToken($configPrefix = ''): AccessToken
+    protected static function initializeAccessToken(): AccessToken
     {
 
-        // APILogger::addInfo("Building provider for $configPrefix");
         $provider = new GenericProvider([
-            'clientId' => Config::get($configPrefix . 'OAUTH_CLIENT_ID', null, true),
-            'clientSecret' => Config::get($configPrefix . 'OAUTH_CLIENT_SECRET', null, true),
+            'clientId' => Config::get('OAUTH_CLIENT_ID', null, true),
+            'clientSecret' => Config::get('OAUTH_CLIENT_SECRET', null, true),
             'redirectUri' => '',
-            'urlAuthorize' => Config::get($configPrefix . 'OAUTH_AUTH_URI'),
-            'urlAccessToken' => Config::get($configPrefix . 'OAUTH_TOKEN_URI'),
+            'urlAuthorize' => Config::get('OAUTH_AUTH_URI'),
+            'urlAccessToken' => Config::get('OAUTH_TOKEN_URI'),
             'urlResourceOwnerDetails' => '',
             'scopes' => 'read:patron read:item read:bib readwrite:hold_request'
         ]);
-        // APILogger::addInfo("Built provider for $configPrefix");
 
         return $provider->getAccessToken('client_credentials');
     }
